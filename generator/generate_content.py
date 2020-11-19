0dcee290d5071219a2edcf1b177592d79a247822
Guido Fioravantti <guido.fioravantti@gmail.com>
2020-11-18 23:34:16 -0500

    First rough cut working

--- generator/generate_content.py
+++ generator/generate_content.py
@@ -1,13 +1,14 @@
 from argparse import ArgumentParser
 from argparse import Namespace
 from json import loads
+from os.path import join
 
 parser: ArgumentParser = ArgumentParser(
     description="Grab JSOND and generate content for Pelican.",
 )
 
 parser.add_argument("jsond_data_filepath")
-parser.add_argument("output-content-dir")
+parser.add_argument("output_content_dir")
 
 args: Namespace = parser.parse_args()
 
@@ -16,4 +17,12 @@ with open(args.jsond_data_filepath, "r") as jsond_data:
     data = loads(jsond_data.read())
 
 for item in data["items"]:
-    print(item["parent"]["uniqueId"])
+    unique_id: str = item["parent"]["uniqueId"]
+
+    output_filename: str = join(
+        args.output_content_dir,
+        f"{unique_id}.markdown"
+    )
+
+    with open(output_filename, "w") as content_file:
+        content_file.write(f"title: #{unique_id}")
