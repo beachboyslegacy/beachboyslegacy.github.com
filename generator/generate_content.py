74eaad56672761607e0fff7f1ce132ed13687e7e
Guido Fioravantti <guido.fioravantti@gmail.com>
2020-11-18 23:23:26 -0500

    Manipulate entry data

--- generator/generate_content.py
+++ generator/generate_content.py
@@ -1,13 +1,19 @@
 from argparse import ArgumentParser
 from argparse import Namespace
+from json import loads
 
 parser: ArgumentParser = ArgumentParser(
     description="Grab JSOND and generate content for Pelican.",
 )
 
-parser.add_argument("jsond-data-path")
+parser.add_argument("jsond_data_filepath")
 parser.add_argument("output-content-dir")
 
 args: Namespace = parser.parse_args()
 
-print(args)
+data: dict
+with open(args.jsond_data_filepath, "r") as jsond_data:
+    data = loads(jsond_data.read())
+
+for item in data["items"]:
+    print(item["parent"]["uniqueId"])
