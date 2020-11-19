742f7090763c364e26df6d559ef624e13d299651
Guido Fioravantti <guido.fioravantti@gmail.com>
2020-11-18 23:14:19 -0500

    Slim down imports

--- generator/generate_content.py
+++ generator/generate_content.py
@@ -1,13 +1,13 @@
-import argparse
+from argparse import ArgumentParser
+from argparse import Namespace
 
-parser: argparse.ArgumentParser = argparse.ArgumentParser(
-    prog="generate_content.py",
+parser: ArgumentParser = ArgumentParser(
     description="Grab JSOND and generate content for Pelican.",
 )
 
 parser.add_argument("jsond-data-path")
 parser.add_argument("output-content-dir")
 
-args: dict = parser.parse_args()
+args: Namespace = parser.parse_args()
 
-
+print(args)
