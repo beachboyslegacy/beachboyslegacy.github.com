07b5872ff9fd9970df189e99d1f6d1180e110b8d
Guido Fioravantti <guido.fioravantti@gmail.com>
2020-11-19 22:11:59 -0500

    Artist selection

--- generator/generate_content.py
+++ generator/generate_content.py
@@ -1,24 +1,27 @@
 from argparse import ArgumentParser
 from argparse import Namespace
+from jinja2 import Template
 from json import loads
-from os import listdir
 from os import path
 from shutil import copytree
 from shutil import rmtree
+from typing import Set
 
 parser: ArgumentParser = ArgumentParser(
     description="Grab JSOND and generate a static website.",
 )
 
 parser.add_argument("jsond_data_filepath")
-parser.add_argument("static_resources_dir")
-parser.add_argument("output_dir")
+parser.add_argument("--static-resources-dir")
+parser.add_argument("--output-dir")
+parser.add_argument("--templates-dir")
 
 args: Namespace = parser.parse_args()
 
 jsond_data_filepath: str = args.jsond_data_filepath
-static_resources_dir: str = args.static_resources_dir
-output_dir: str = args.output_dir
+static_resources_dir: str = args.static_resources_dir or "./static_resources"
+output_dir: str = args.output_dir or "./www"
+templates_dir: str = args.templates_dir or "./templates"
 
 # Load data into memmory.
 data: dict
@@ -30,3 +33,16 @@ rmtree(output_dir)
 
 # Move static resources into place first.
 copytree(static_resources_dir, output_dir)
+
+# Process index template.
+index_template_path: str = path.join(templates_dir, "index.html.jinja2")
+with open(index_template_path, "r") as index_template:
+    template: Template = Template(index_template.read())
+
+# Let's get a list of unique artists.
+artists: Set = sorted(set([
+    item["parent"].get("byArtist", "The Beach Boys") for item in data["items"]
+]))
+
+with open(path.join(output_dir, "index.html"), "w") as index_file:
+    index_file.write(template.render(artists=artists))
