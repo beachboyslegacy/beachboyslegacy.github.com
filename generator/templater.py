4c2924d730bd9de83d7a38d3911102b6d4ae27cf
Guido Fioravantti <guido.fioravantti@gmail.com>
2020-12-23 23:08:57 -0500

    Add list method

--- generator/templater.py
+++ generator/templater.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from jinja2 import Template
+from os import walk
 from os.path import basename
 from os.path import join
 from os.path import splitext
@@ -29,3 +30,20 @@ class Templater:
             self.cache[filepath] = template
 
         return (name, template)
+
+    def list(self, subdir=None) -> list[tuple[str, Template]]:
+        """Lists all templates in a subdir (or all templates by default).
+
+        Arguments:
+        subdir: str -- directory in which to search for templates
+            (default self.templates_dir).
+        """
+        parsed_subdir: str = join(self.templates_dir, subdir or "")
+
+        template_paths: list[str] = []
+        for path, dirnames, filenames in walk(parsed_subdir):
+            # Find the leaves of the tree.
+            if filenames and not dirnames:
+                template_paths.extend(filenames)
+
+        return [self.get(template_path) for template_path in template_paths]
