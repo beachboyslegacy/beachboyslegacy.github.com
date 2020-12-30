b060dd4818fd598754624ddb0b89a5f31292926f
Guido Fioravantti <guido.fioravantti@gmail.com>
2020-12-29 23:47:50 -0500

    Add base url config

--- cli.py
+++ cli.py
@@ -14,6 +14,7 @@ parser.add_argument("jsond_data_filepath")
 parser.add_argument("--static-resources-dir")
 parser.add_argument("--output-dir")
 parser.add_argument("--templates-dir")
+parser.add_argument("--base-url")
 
 args: Namespace = parser.parse_args()
 
@@ -21,10 +22,12 @@ jsond_data_filepath: str = args.jsond_data_filepath
 static_resources_dir: str = args.static_resources_dir
 output_dir: str = args.output_dir or "./www"
 templates_dir: str = args.templates_dir or "./templates"
+base_url: str = args.templates_dir or "https://beachboyslegacy.com"
 
 Generator(
     jsond_data_filepath=jsond_data_filepath,
     static_resources_dir=static_resources_dir,
     output_dir=output_dir,
     templates_dir=templates_dir,
+    base_url=base_url,
 ).generate()
