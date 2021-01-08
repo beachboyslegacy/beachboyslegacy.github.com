4d03be9361a7fe412b7c47cf3eaafe0e81160cd9
Guido Fioravantti <guido.fioravantti@gmail.com>
2021-01-08 01:08:26 -0500

    Add sorting script

---
+++ js/sort_items.js
@@ -0,0 +1,30 @@
+function sortItems(order) {
+    const items_list = document.getElementById("items");
+    const sorted_items_list = items_list.cloneNode(false);
+
+    const items = [...items_list.children];
+    items.sort((thisItem, thatItem) => (
+        order*(parseInt(thatItem.dataset.date) - parseInt(thisItem.dataset.date))
+
+    ));
+
+    for (item of items) {
+        sorted_items_list.append(item);
+    }
+
+    items_list.parentNode.replaceChild(sorted_items_list, items_list);
+}
+
+window.addEventListener("load", () => {
+    document
+        .getElementById("sorting-release")
+        .addEventListener("click", () => {
+            itemsOrder = sortItems();
+        });
+
+    const sortingButton = document.getElementById("sorting-release");
+    sortingButton.addEventListener("click", () => {
+        sortItems(parseInt(sortingButton.dataset.order));
+        sortingButton.dataset.order *= -1;
+    });
+});
