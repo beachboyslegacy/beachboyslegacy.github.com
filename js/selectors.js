c16624238fef1fa6833b9270f3a9a66ee71c4bd1
Guido Fioravantti <guido.fioravantti@gmail.com>
2021-01-20 00:17:34 -0500

    Remove set height code

--- js/selectors.js
+++ js/selectors.js
@@ -1,16 +1,7 @@
 window.addEventListener("load", () => {
-    var artistSelectorHeight = $('#artist').height() + 20;
-    var categorySelectorHeight = $('#category').height() + 20;
-    $('#artist').css({
-      'min-height':artistSelectorHeight
+    $('.selector__current').on( 'click', function() {
+        $(this).next('.selector__options')
+            .slideToggle(200)
+            .toggleClass('open-menu');
     });
-    $('#category').css({
-      'min-height':categorySelectorHeight
-    });
-});
-
-$('.selector__current').on( 'click', function() {
-    $(this).next('.selector__options')
-        .slideToggle(200)
-        .toggleClass('open-menu');
 });
