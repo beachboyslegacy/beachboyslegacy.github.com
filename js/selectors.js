d9bebdfeb4243a3c1338332fabc31ce611c8846a
Guido Fioravantti <guido.fioravantti@gmail.com>
2021-02-16 23:18:56 -0500

    delete everything

--- js/selectors.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", () => {
-    $('.selector__current').on( 'click', function() {
-        $(this).next('.selector__options')
-            .slideToggle(200)
-            .toggleClass('open-menu');
-    });
-});
